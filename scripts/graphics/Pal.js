60ff78b881768c6e24a9df92ace8cb35b11308e8
NemesisTheory <89642216+NemesisTheory@users.noreply.github.com>
2023-06-07 20:37:21 +0700

    Update Pal.js

--- scripts/graphics/Pal.js
+++ scripts/graphics/Pal.js
@@ -1,9 +1,13 @@
 Events.on(ClientLoadEvent, b => {
   Pal.darkOutline = Color.valueOf("14121b");
+  
   Pal.heal = Color.valueOf("ffde83");
+  
   Pal.sap = Color.valueOf("ffde83");
   Pal.sapBullet = Color.valueOf("ffde83");
   Pal.sapBulletBack = Color.valueOf("ffde83");
+  
   Pal.suppress = Color.valueOf("ffde83");
+  
   Pal.techBlue = Color.valueOf("ffde83");
 });
